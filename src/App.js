537ec56c4519b96394f372b0f252f551cf9b6900
Jason Shih <jason_shih@hotmail.com>
2024-02-11 16:15:54 -0800

    Add stopStream

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import BarcodeScannerComponent from "react-qr-barcode-scanner-updated";
 
 function App() {
-  const [data, setData] = React.useState("Not Found");
+  const [data, setData] = useState("Not Found");
+  const [stopStream, setStopStream] = useState(false)
 
   return (
     <>
       <h1>Hello</h1>
+      <button onClick={() => setStopStream(prev => !prev)}>Stop Stream={stopStream ? "true" : "false"}</button>
       <BarcodeScannerComponent
         width={500}
         height={500}
+        stopStream={stopStream}
         onUpdate={(err, result) => {
-          if (result) setData(result.text);
+          if (result) {
+            setData(result.text);
+            setStopStream(true)
+          }
           else setData("Not Found");
         }}
       />
